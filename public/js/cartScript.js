a9fd956daa051eed8613d131b568451d9b816987
Jatin Bahl <77239924+Jatin-Bahl@users.noreply.github.com>
2023-12-22 23:45:54 +0530

    Update cartScript.js

--- public/js/cartScript.js
+++ public/js/cartScript.js
@@ -1,35 +1,89 @@
 let cart = [];
-let total = 0;
+        let total = 0;
 
-function addToCart(productName, price) {
-    cart.push({ name: productName, price: price });
-    total += price;
 
-    updateCart();
-}
+        function addToCart(productName, price) {
+            cart.push({ name: productName, price: price });
+            total += price;
+            updateCart();
+            updateTotalForPaymentGateway();
+        }
+        function updateTotalForPaymentGateway() {
+            const rzpButton = document.getElementById('rzp-button');
+            
+            // Update the total in paise
+            const totalInPaise = Math.ceil(parseFloat(total) * 100);
+            console.log("Total in paise:", totalInPaise);
 
-function updateCart() {
-    const cartList = document.getElementById('cart-list');
-    const totalElement = document.getElementById('total');
+            // Update the total amount in the payment gateway options
+            options.amount = totalInPaise;
 
-    // Clear the cart list
-    cartList.innerHTML = '';
+            // You may need to recreate the Razorpay instance with updated options
+            const rzpInstance = new Razorpay(options);
+            
+            // Update the click event listener
+            rzpButton.addEventListener('click', function () {
+                rzpInstance.open();
+            });
+        }
+        const rzpKey = 'enter_your_razorpay_key';
+        
+        const options = {
+            key: rzpKey,
+            amount: Math.ceil(total * 100), 
+            currency: 'INR',
+            name: 'StyleSphere',
+            description: 'Payment for your order',
+            handler: function (response) {
+                alert('Payment successful!'); 
+            },
+            prefill: {
+                name: 'Customer Name',
+                email: 'customer@example.com',
+                contact: '1234567890',
+            },
+            notes: {
+                address: 'Your Store Address',
+            },
+            theme: {
+                color: '#a4916d', 
+            },
+        };
+    
+        const rzpButton = document.getElementById('rzp-button');
+        rzpButton.addEventListener('click', function () {
+            const rzpInstance = new Razorpay(options);
+            rzpInstance.open();
+        });
 
-    // Populate the cart list
-    cart.forEach(item => {
-        const li = document.createElement('li');
-        li.textContent = `${item.name} - $${item.price.toFixed(2)}`;
-        cartList.appendChild(li);
-    });
 
-    // Update the total
-    totalElement.textContent = total.toFixed(2);
+        function updateCart() {
+            const cartList = document.getElementById('cart-items');
+            const totalElement = document.getElementById('total');
 
-    // Display the cart container
-    const cartContainer = document.getElementById('cart-container');
-    cartContainer.style.display = 'block';
+            // Clear the cart list
+            cartList.innerHTML = '';
 
-    // Update the cart count in the header
-    const cartCount = document.getElementById('cart');
-    cartCount.textContent = `Cart (${cart.length})`;
-}
+            // Populate the cart list
+            cart.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = `${item.name} - ₹${item.price.toFixed(2)}`;
+                cartList.appendChild(li);
+            });
+
+            // Update the total
+            totalElement.textContent = `Total: ₹${total.toFixed(2)}`;
+
+            // Display the cart container
+            const cartContainer = document.getElementById('cart-container');
+            cartContainer.style.display = 'block';
+
+            // Update the cart count in the header
+            const cartCount = document.querySelector('.cart-count');
+            cartCount.textContent = `(${cart.length})`;
+        }
+
+        function toggleCart() {
+            const cartContainer = document.getElementById('cart-container');
+            cartContainer.style.display = (cartContainer.style.display === 'none') ? 'block' : 'none';
+        }
